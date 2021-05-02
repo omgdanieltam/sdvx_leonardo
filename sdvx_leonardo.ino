573e246bc24039c183b59826e81905fd644e9eea
Henry Chen <chenx97@aosc.io>
2021-05-02 05:44:35 -0700

    meta: sanitize code; avoid float conversion; use controller default binding

--- sdvx_leonardo.ino
+++ sdvx_leonardo.ino
@@ -1,13 +1,16 @@
 /*
-* By Daniel Tam (daniel@danieltam.net)
-============ NOTES ============
-This will create a SDVX Controller using an Arduino Leonardo. 
-You will hook up one wire of the button to the Arduino pin and the other to Arduino's ground. When pushed, the button should go to ground.
-You will need to download the encoder library and use it in your project for this to work (ww.circuitsathome.com/mcu/reading-rotary-encoder-on-arduino)
-===============================
+  By Daniel Tam (daniel@danieltam.net)
+  modified by Henry Chen <chenx97@aosc.io>
+  ============ NOTES ============
+  This will create a SDVX Controller using an Arduino Leonardo.
+  You will hook up one wire of the button to the Arduino pin and the other to Arduino's ground. When pushed, the button should go to high.
+  You will need to download the encoder library and use it in your project for this to work (ww.circuitsathome.com/mcu/reading-rotary-encoder-on-arduino)
+  ===============================
 */
 
 // buttons
+#include <Mouse.h>
+#include <Keyboard.h>
 #define BT_A 4
 #define BT_B 5
 #define BT_C 6
@@ -17,18 +20,18 @@ You will need to download the encoder library and use it in your project for thi
 #define BT_ST 10
 
 // encoders
-#include <Encoder.h>
+#include "Encoder.h"
 Encoder enc1(0, 1);
 Encoder enc2(2, 3);
-float knob1 = 0;
-float knob2 = 0;
-float old_knob1 = 0;
-float old_knob2 = 0;
+int16_t knob1 = 0;
+int16_t knob2 = 0;
+int8_t old_knob1 = 0;
+int8_t old_knob2 = 0;
 
 void setup()
-{ 
+{
   // set pins to read, and output high
-  // when the pins read low, we know that the buttons have went to ground (aka, pushed down)
+  // when the pins read low, we know that the buttons have been grounded (aka, pushed down)
   pinMode(BT_A, INPUT);
   pinMode(BT_B, INPUT);
   pinMode(BT_C, INPUT);
@@ -36,7 +39,7 @@ void setup()
   pinMode(FX_L, INPUT);
   pinMode(FX_R, INPUT);
   pinMode(BT_ST, INPUT);
-  
+
   digitalWrite(BT_A, HIGH);
   digitalWrite(BT_B, HIGH);
   digitalWrite(BT_C, HIGH);
@@ -44,102 +47,22 @@ void setup()
   digitalWrite(FX_L, HIGH);
   digitalWrite(FX_R, HIGH);
   digitalWrite(BT_ST, HIGH);
-  
+
   Mouse.begin();
   Keyboard.begin();
+  Keyboard.releaseAll();
 }
 
 void loop()
 {
   // read encoders
-  knob1 =  (float)(enc1.read());
-  knob2 = (float)enc2.read();
-  
-  if(knob1 != old_knob1)
-  {
-    // if there's a difference in encoder movement from last pass, move the mouse
-    if(knob1 < old_knob1)
-    {
-      Mouse.move(0, -5);
-    }
-    else
-    {
-      Mouse.move(0, 5);
-    }
-    
-	// we count the difference in the encoders, but we must not go over arduino's int limit
-    if(knob1 < -255)
-    {
-      enc1.write(0);
-      old_knob1 = 0;
-    }
-    else if (knob1 > 255)
-    {
-      enc1.write(0);
-      old_knob1 = 0;
-    }
-    else
-    {
-      old_knob1 = knob1;
-    }
-  }
-  
-  if(knob2 != old_knob2)
-  {
-    if(knob2 > old_knob2)
-    {
-      Mouse.move(-5, 0);
-    }
-    else
-    {
-      Mouse.move(5, 0);
-    }
-    
-    if(knob2 < -255)
-    {
-      enc2.write(0);
-      old_knob2 = 0;
-    }
-    else if(knob2 > 255)
-    {
-      enc2.write(0);
-      old_knob2 = 0;
-    }
-    else
-    {
-      old_knob2 = knob2;
-    }
-  }
-  
-  // read the buttons for low, if it's low, output a keyboard press  
-  if(digitalRead(BT_A) == LOW)
-  {
-    Keyboard.press('a');
-  }
-  else
-  {
-    Keyboard.release('a');
-  }
-  
-  if(digitalRead(BT_B) == LOW)
-  {
-    Keyboard.press('b');
-  }
-  else
-  {
-    Keyboard.release('b');
-  }
-  
-  if(digitalRead(BT_C) == LOW)
-  {
-    Keyboard.press('c');
-  }
-  else
-  {
-    Keyboard.release('c');
-  }
-  
-  if(digitalRead(BT_D) == LOW)
+  knob1 = enc1.read();
+  knob2 = enc2.read();
+
+  update_knobs();
+
+  // read the buttons for high, if it's low, output a keyboard press
+  if (digitalRead(BT_A) == LOW)
   {
     Keyboard.press('d');
   }
@@ -147,31 +70,97 @@ void loop()
   {
     Keyboard.release('d');
   }
-  
-  if(digitalRead(FX_L) == LOW)
+
+  if (digitalRead(BT_B) == LOW)
   {
-    Keyboard.press('z');
+    Keyboard.press('f');
   }
   else
   {
-    Keyboard.release('z');
+    Keyboard.release('f');
   }
-  
-  if(digitalRead(FX_R) == LOW)
+
+  if (digitalRead(BT_C) == LOW)
   {
-    Keyboard.press('x');
-  }
-  else
-  {
-    Keyboard.release('x');
-  }
-  
-  if(digitalRead(BT_ST) == LOW)
-  {
-    Keyboard.press('n');
+    Keyboard.press('j');
   }
   else
   {
-    Keyboard.release('n');
+    Keyboard.release('j');
+  }
+
+  if (digitalRead(BT_D) == LOW)
+  {
+    Keyboard.press('k');
+  }
+  else
+  {
+    Keyboard.release('k');
+  }
+
+  if (digitalRead(FX_L) == LOW)
+  {
+    Keyboard.press('c');
+  }
+  else
+  {
+    Keyboard.release('c');
+  }
+
+  if (digitalRead(FX_R) == LOW)
+  {
+    Keyboard.press('m');
+  }
+  else
+  {
+    Keyboard.release('m');
+  }
+
+  if (digitalRead(BT_ST) == LOW)
+  {
+    Keyboard.press('1');
+  }
+  else
+  {
+    Keyboard.release('1');
+  }
+}
+
+void update_knobs()
+{
+  if (knob1 != old_knob1)
+  {
+    // if there's a difference in encoder movement from last pass, move the mouse
+    if (knob1 < old_knob1)
+    {
+      Mouse.move(0, -5);
+    }
+    else
+    {
+      Mouse.move(0, 5);
+    }
+
+    // if overflown, then write back to the encoder
+    if ((old_knob1 = knob1) != knob1)
+    {
+      enc1.write(old_knob1);
+    }
+  }
+  if (knob2 != old_knob2)
+  {
+    if (knob2 > old_knob2)
+    {
+      Mouse.move(-5, 0);
+    }
+    else
+    {
+      Mouse.move(5, 0);
+    }
+
+    // if overflown, then write back to the encoder
+    if ((old_knob2 = knob2) != knob2)
+    {
+      enc2.write(old_knob2);
+    }
   }
 }
